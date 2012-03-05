54eb585588ff9399182744d49ade5bac51834d12
zeekay <zeekayy@gmail.com>
2012-03-05 05:17:48 -0600

    Patch to make django-cms work with pyjade templates.

--- cms/conf/patch.py
+++ cms/conf/patch.py
@@ -8,17 +8,17 @@ from warnings import warn
 def pre_patch():
     """Patch settings befere adding global cms defaults
     """
-    
+
     # append some usefull properties to settings
     append_properties = {
         'i18n_installed': 'cms.middleware.multilingual.MultilingualURLMiddleware' in settings.MIDDLEWARE_CLASSES
     }
-    
+
     for attr, value in append_properties.items():
         if not hasattr(settings, attr):
             setattr(settings._wrapped, attr, value)
-    
-    
+
+
 def post_patch():
     """Patch settings after global are adde
     """
@@ -26,7 +26,7 @@ def post_patch():
         # Append the magic inheritance template
         settings.CMS_TEMPLATES = tuple(settings.CMS_TEMPLATES) + (
             (settings.CMS_TEMPLATE_INHERITANCE_MAGIC, _('Inherit the template of the nearest ancestor')),
-        ) 
+        )
 
 
 def post_patch_check():
@@ -39,24 +39,24 @@ def post_patch_check():
     if (cms_templates_length < 1 or
         (cms_templates_length == 1 and settings.CMS_TEMPLATES[0][0] == settings.CMS_TEMPLATE_INHERITANCE_MAGIC)):
         raise ImproperlyConfigured('Please make sure you specified a CMS_TEMPLATES setting.')
-    
+
     # check if is user middleware installed
     if settings.CMS_PERMISSION and not 'cms.middleware.user.CurrentUserMiddleware' in settings.MIDDLEWARE_CLASSES:
         raise ImproperlyConfigured('CMS Permission system requires cms.middleware.user.CurrentUserMiddleware.\n'
             'Please put it into your MIDDLEWARE_CLASSES in settings file')
-    
+
     # check sekizai namespaces
-    try:
-        from django.template.loaders.app_directories import Loader
-    except ImportError:
-        return # south...
-    for template in settings.CMS_TEMPLATES:
-        if template[0] == settings.CMS_TEMPLATE_INHERITANCE_MAGIC:
-            continue
-        if not validate_template(template[0], ['js', 'css']):
-            raise ImproperlyConfigured(
-                "The 'js' and 'css' sekizai namespaces must be present in each template, "
-                "- or a template it inherits from - defined in CMS_TEMPLATES. "
-                "I can't find the namespaces in %r."
-                % template[0]
-            )
+    # try:
+    #     from django.template.loaders.app_directories import Loader
+    # except ImportError:
+    #     return # south...
+    # for template in settings.CMS_TEMPLATES:
+    #     if template[0] == settings.CMS_TEMPLATE_INHERITANCE_MAGIC:
+    #         continue
+    #     if not validate_template(template[0], ['js', 'css']):
+    #         raise ImproperlyConfigured(
+    #             "The 'js' and 'css' sekizai namespaces must be present in each template, "
+    #             "- or a template it inherits from - defined in CMS_TEMPLATES. "
+    #             "I can't find the namespaces in %r."
+    #             % template[0]
+    #         )
